0252991e643c98589d217efe2bd0100c900fd642
Taiyan Hao <th1577@nyu.edu>
2016-05-05 22:30:08 -0400

    First Commit

---
+++ DeliveryMan/DeliveryMan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeliveryMan.MvcApplication" Language="C#" %>
---
+++ DeliveryMan/DeliveryMan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeliveryMan.Startup))]
+namespace DeliveryMan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
