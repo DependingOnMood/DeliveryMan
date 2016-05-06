a0b031fa09389df612ed8813c12e0fea2126e618
Weihang Song <mirrorlol501@gmail.com>
2016-05-05 23:00:15 -0400

    Add Restaurant and RestaurantContext classes.

---
+++ DeliveryMan/DataLayer/Restaurant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Restaurant
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int ContactId { get; set; }
+
+        [ForeignKey("ContactId")]
+        public virtual Contact Contact { get; set; }
+
+        [Url]
+        [Display(Name = "Insert your icon image URL.")]
+        public string IconImageUrl { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Deliveryman> BadDeliverymen { get; set; }
+    }
+}
