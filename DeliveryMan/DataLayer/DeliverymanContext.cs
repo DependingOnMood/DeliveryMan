aee04d317c61c16d0f9bbd8680ac90eabf562a4c
Weihang Song <mirrorlol501@gmail.com>
2016-05-05 22:43:35 -0400

    NuGet managed EntityFrameWork package,  and add two datalayer classes.

---
+++ DeliveryMan/DataLayer/DeliverymanContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DeliverymanContext : DbContext
+    {
+        public DeliverymanContext()
+        {
+        }
+
+        public virtual DbSet<Deliveryman> Deliverymen { get; set; }
+    }
+}
