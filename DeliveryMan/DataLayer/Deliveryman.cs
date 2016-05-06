7de7bb2ef900d0eaad8a0d212b1f1e2cb8e19bd4
Weihang Song <mirrorlol501@gmail.com>
2016-05-05 23:05:13 -0400

    Add Deliveryman class.

---
+++ DeliveryMan/DataLayer/Deliveryman.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+        public class Deliveryman
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            public int ContactId { get; set; }
+
+            [ForeignKey("ContactId")]
+            public virtual Contact Contact { get; set; }
+
+            [Url]
+            [Display(Name = "Insert your icon image URL.")]
+            public string IconImageUrl { get; set; }
+
+            public int TotalDeliveryCount { get; set; }
+
+            public int TotalStarsEarned { get; set; }
+
+            public int Rating { get; set; }
+
+            public int Ranking { get; set; }
+
+            public decimal Balance { get; set; }
+        }
+    }
+}
