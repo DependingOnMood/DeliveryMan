506509d54caac5242840742f0169e373edc8dca3
Weihang Song <mirrorlol501@gmail.com>
2016-05-05 23:25:28 -0400

    Add Destination and DestinationContext classes.

---
+++ DeliveryMan/DataLayer/Destination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Destination
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int ContactId { get; set; }
+
+        [ForeignKey("ContactId")]
+        public virtual Contact Contact { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ DeliveryMan/DataLayer/DestinationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DestinationContext : DbContext
+    {
+        public DestinationContext()
+        {
+        }
+
+        public virtual DbSet<Destination> Destinations { get; set; }
+    }
+}
