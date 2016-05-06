edc70468b3292566b04ecd916dce15c6a4dda6ad
Dongbo Xiao <xiaodongbo627@hotmail.com>
2016-05-05 22:38:47 -0400

    added a few models

---
+++ DeliveryMan/DataLayer/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Delivery.Models
+{
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public Role Role { get; set; }
+
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public Address Address { get; set; }
+    }
+
+    public enum Role
+    {
+        DELIVERYMAN,
+        RESTAUTANT,
+        CUSTOMER,
+    }
+}
