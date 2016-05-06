bebab9fdf68d34e686c58cb86336110cb919d0a9
Weihang Song <mirrorlol501@gmail.com>
2016-05-05 23:14:54 -0400

    Add Order and OrderContext classes.

---
+++ DeliveryMan/DataLayer/Order.cs
@@ -0,0 +1,55 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int RestaurantId { get; set; }
+
+        [ForeignKey("RestaurantId")]
+        public virtual Restaurant Restaurant { get; set; }
+
+        public int DeliverymanId { get; set; }
+
+        [ForeignKey("DeliverymanId")]
+        public virtual Restaurant Deliveryman { get; set; }
+
+        public int DestinationId { get; set; }
+
+        [ForeignKey("DestinationId")]
+        public virtual Restaurant Destination { get; set; }
+
+        public Status Status { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime PlacedTime { get; set; }
+
+        public DateTime PickUpTime { get; set; }
+
+        public DateTime DeliveredTime { get; set; }
+
+        public TimeSpan ETA { get; set; }
+
+        public decimal DeliveryFee { get; set; }
+    }
+
+    public enum Status
+    {
+        WAITING,
+        PENDING,
+        INPROGRESS,
+        DELIVERED,
+    }
+}
